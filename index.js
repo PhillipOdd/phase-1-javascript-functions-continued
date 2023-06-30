79b3fc9a4bad706cd7af521f8b652147c8a1d40e
Phillip Lloyd <phillip.lloyd@student.moringaschool.com>
2023-06-30 06:44:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function demoChain(name) {
+    const part1 = "hi";
+    return function () {
+        const part2 = "there";
+        return function () {
+            console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+        };
+    };
+}
+
+demoChain("Dr. Stephen Strange")()(); //=> HI there Dr. Stephen Strange
